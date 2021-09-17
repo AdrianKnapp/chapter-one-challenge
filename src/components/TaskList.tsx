22e216f4cc233d7f934253b92a8e841e82114395
Adrian <adrknapp@gmail.com>
2021-09-16 21:15:12 -0300

    Challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,31 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle.length) {
+      const taskId = Math.trunc(Math.random() * 100);
+      const newTask= {
+        id: taskId,
+        title: newTaskTitle,
+        isComplete: false 
+      }
+      setTasks([...tasks, newTask]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    tasks.forEach(task => {
+      if(task.id === id) {
+        task.isComplete = !task.isComplete;
+        setTasks([...tasks]);
+      }
+    });
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter(task => task.id !== id);
+    setTasks(newTasks);
   }
 
   return (
